90a6562cdfac3988121cda19fe285f06e07e13dc
Pablo Repetto <caussabon@gmail.com>
2017-08-28 18:08:05 -0300

    Added addToRoot test.

--- test/UNIT_bintree.c
+++ test/UNIT_bintree.c
@@ -4,15 +4,16 @@
 #include <assert.h>
 #include "../src/bintree.h"
 
-int tests_run = 0;
-
 static void noop(void *object);
 static void test_errorAlreadyHaslchild();
+static void test_addToRoot();
 
 int main(int argc, char **argv) {
     test_errorAlreadyHaslchild();
+    test_addToRoot();
     printf("All tests ok.\n");
 }
+
 static void test_errorAlreadyHaslchild() {
     BinaryTree *tree = BinaryTree_new(NULL, NULL, NULL);
 
@@ -21,6 +22,19 @@ static void test_errorAlreadyHaslchild() {
 
     return_value = BinaryTree_newLeft(tree, NULL);
     assert(return_value == -1);
+
+    BinaryTree_delete(tree, noop);
+}
+
+static void test_addToRoot() {
+    char a = 'a';
+    char b = 'b';
+
+    BinaryTree *tree = BinaryTree_new(&a, NULL, NULL);
+    tree = BinaryTree_new(&b, tree, NULL);
+
+    assert('b' == *((char*) BinaryTree_getContent(tree)));
+    assert('a' == *((char*) BinaryTree_getContent(BinaryTree_lchild(tree))));
 
     BinaryTree_delete(tree, noop);
 }
