56ead25bb6faf965dff711a1ff72c78bfefefbb6
Pablo Repetto <caussabon@gmail.com>
2017-09-05 16:27:47 -0300

    main now checks parameters

--- src/main.c
+++ src/main.c
@@ -8,16 +8,32 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
+static void printHelp();
+static void serve(int argc, char **argv);
 static int createListeningSocket(char *port);
 static void echoSocket(const int fd);
 
 int main(int argc, char **argv) {
-    int listening = createListeningSocket(argv[1]);
+    if (argc < 2) { printHelp(); exit(0); }
+
+    if (strcmp(argv[1], "server") == 0) serve(argc, argv);
+    //if (strcmp(argv[1], "client") == 0) connect(argc, argv);
+}
+
+static void printHelp() {
+    printf("./tp​ ​server​ ​[<port>]\n"
+           "./tp​ ​ client​ ​ <host>​ ​ <port>​ ​ [<input>]\n");
+}
+
+static void serve(int argc, char **argv) {
+    if (argc > 3) { printHelp(); exit(1); }
+    int listening = createListeningSocket(argv[2] ? argv[2] : "8080");
 
     while (1) {
         struct accept_s ret = Socket_accept(.fd=listening);
-        if (ret.error != 0) { perror("Accept failed"); break; };
+        if (ret.error != 0) { perror("Accept failed"); break; }
 
         int connection = ret.fd;
         echoSocket(connection);
@@ -33,19 +49,19 @@ static int createListeningSocket(char *port) {
     port = port ? port : "8080";
 
     int fd = Socket_socket();
-    if (fd == -1) { perror("Could not open socket"); abort(); }
+    if (fd == -1) { perror("Could not open socket"); exit(1); }
 
     struct addrinfo *res;
     int errcode = Socket_getaddrinfo(.res=&res, .service=port);
     if (errcode) {
         perror("Could not get local address");
-        abort();
+        exit(1);
     }
 
-    if (Socket_bind(.fd=fd, .addr=res)) { perror("Could not bind"); abort(); }
+    if (Socket_bind(.fd=fd, .addr=res)) { perror("Could not bind"); exit(1); }
     Socket_freeaddrinfo(.res=res);
 
-    if (Socket_listen(.fd=fd)) { perror("Could not listen"); abort(); }
+    if (Socket_listen(.fd=fd)) { perror("Could not listen"); exit(1); }
 
     return fd;
 }
@@ -56,7 +72,7 @@ static void echoSocket(const int connection) {
     int n;
     do {
         n = Socket_recv(.fd=connection, .buf=buf, .len=SIZE);
-        if (write(1, buf, n) == -1) { perror("Failed to write"); abort(); };
+        if (write(1, buf, n) == -1) { perror("Failed to write"); exit(1); }
     } while (n == SIZE);
     #undef SIZE
 }
