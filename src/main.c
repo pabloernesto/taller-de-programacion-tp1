75dda7feb782d4e621f40ec27d933e543e3c2b11
Pablo Repetto <caussabon@gmail.com>
2017-09-05 16:12:35 -0300

    Refactored main into a more rational echo program.

--- src/main.c
+++ src/main.c
@@ -3,43 +3,60 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
-
 #include "socket.h"
+
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+
+static int createListeningSocket(char *port);
+static void echoSocket(const int fd);
 
 int main(int argc, char **argv) {
-    int fd = Socket_socket();
-    if (fd == -1) { perror("Could not open socket.\n"); return 1; }
-
-    struct addrinfo *res;
-    char *port = argv[1] ? argv[1] : "8080";
-    int errcode = Socket_getaddrinfo(.res=&res, .service=port);
-    if (errcode) {
-        perror("Could not get local address: ");
-        perror(Socket_gai_strerror(.errcode=errcode));
-        return 1;
-    }
-
-    if (Socket_bind(.fd=fd, .addr=res)) { perror("Could not bind."); return 1; }
-    Socket_freeaddrinfo(.res=res);
-
-    if (Socket_listen(.fd=fd)) { perror("Could not listen."); return 1; }
+    int listening = createListeningSocket(argv[1]);
 
     while (1) {
-        struct accept_s ret = Socket_accept(.fd=fd);
-        if (ret.error == 0) break;
+        struct accept_s ret = Socket_accept(.fd=listening);
+        if (ret.error != 0) { perror("Accept failed"); break; };
 
-        char buf[256];
-        int n;
-        do {
-            n = Socket_recv(.fd=ret.fd, .buf=buf, .len=256);
-            if (write(1, buf, n) == -1) return 1;
-        } while (n == 256);
+        int connection = ret.fd;
+        echoSocket(connection);
 
-        Socket_shutdown(.fd=ret.fd);
-        Socket_close(.fd=ret.fd);
+        Socket_shutdown(.fd=connection);
+        Socket_close(.fd=connection);
     }
 
-    Socket_close(.fd=fd);
+    Socket_close(.fd=listening);
+}
+
+static int createListeningSocket(char *port) {
+    port = port ? port : "8080";
+
+    int fd = Socket_socket();
+    if (fd == -1) { perror("Could not open socket"); abort(); }
+
+    struct addrinfo *res;
+    int errcode = Socket_getaddrinfo(.res=&res, .service=port);
+    if (errcode) {
+        perror("Could not get local address");
+        abort();
+    }
+
+    if (Socket_bind(.fd=fd, .addr=res)) { perror("Could not bind"); abort(); }
+    Socket_freeaddrinfo(.res=res);
+
+    if (Socket_listen(.fd=fd)) { perror("Could not listen"); abort(); }
+
+    return fd;
+}
+
+static void echoSocket(const int connection) {
+    #define SIZE 256
+    char buf[SIZE];
+    int n;
+    do {
+        n = Socket_recv(.fd=connection, .buf=buf, .len=SIZE);
+        if (write(1, buf, n) == -1) { perror("Failed to write"); abort(); };
+    } while (n == SIZE);
+    #undef SIZE
 }
